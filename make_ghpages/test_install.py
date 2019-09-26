e21f02096e7e09ab3bb57e5d91b2631512877b19
Leopold Talirz <leopold.talirz@gmail.com>
2019-09-26 23:14:41 +0200

    split off install test on travis

--- make_ghpages/test_install.py
+++ make_ghpages/test_install.py
@@ -7,6 +7,10 @@ from __future__ import print_function
 import json
 import six
 import subprocess
+import os
+
+pwd = os.path.split(os.path.abspath(__file__))[0]
+PLUGINS_FILE_ABS = os.path.join(pwd, os.pardir, 'plugins.json')
 
 
 def try_cmd(cmd):
@@ -16,13 +20,13 @@ def try_cmd(cmd):
         print(exc.output)
 
 
-with open('all_data.json', 'r') as handle:
+with open(PLUGINS_FILE_ABS, 'r') as handle:
     data = json.load(handle)
 
 if __name__ == "__main__":
 
     print("[test installing plugins]")
-    for k, v in six.iteritems(data['plugins']):
+    for k, v in six.iteritems(data):
 
         # 'registered' plugins aren't installed/tested
         if v['state'] != 'registered':
@@ -30,6 +34,7 @@ if __name__ == "__main__":
             print(" - Installing {}".format(v['name']))
             try_cmd("pip install {}".format(v['pip_url']))
 
-            print(" - Importing {}".format(v['name']))
-            try_cmd("python -c 'import {}'".format(
-                v['setup_json']['package_name']))
+            if 'package_name' not in list(v.keys()):
+                v['package_name'] = v['name'].replace('-', '_')
+            print(" - Importing {}".format(v['package_name']))
+            try_cmd("python -c 'import {}'".format(v['package_name']))
