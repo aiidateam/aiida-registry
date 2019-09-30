5aa2292175c73d20b3f69b95a5f604c4f312e9fa
Leopold Talirz <leopold.talirz@gmail.com>
2019-09-30 12:32:41 +0200

    fix deploy script
    deploy script thought there were no changes and did not push...

--- .travis-data/deploy.sh
+++ .travis-data/deploy.sh
@@ -50,15 +50,15 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 cp -r ../../make_ghpages/out/* .
 
+git add -A .
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
+if git diff --cached --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Now that we're all set up, we can push.
