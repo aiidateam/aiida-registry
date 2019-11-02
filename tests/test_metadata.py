c598663b1d324d30c7dcd90d17969dc947d1393c
Leopold Talirz <leopold.talirz@gmail.com>
2019-11-03 00:21:52 +0100

    add --pre to install cmd for pre-release versions (#105)
    * add --pre to install cmd for pre-release versions
    
    pip won't install pre-releases by default.
    if the latest version of a plugin is a pre-release, add --pre option to
    pip installation command
    
    Also:
      * adapt css style
      * add test
      * update github workflow

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -8,6 +8,7 @@ This fetches metadata from plugins defined using different build systems
 """
 from __future__ import absolute_import
 from __future__ import print_function
+from . import TEST_DATA
 
 
 def test_entrypoint(capsys):
@@ -15,7 +16,6 @@ def test_entrypoint(capsys):
 
     The registry should check only entry points from the 'aiida.' groups.
     """
-    from . import TEST_DATA
     from aiida_registry.fetch_metadata import validate_plugin_entry_points
 
     # aiida-gulp example contains 'reaxff' entry point in 'gulp.potentials' group
@@ -24,3 +24,14 @@ def test_entrypoint(capsys):
     captured = capsys.readouterr()
     # should not warn about 'reaxff' not starting with 'gulp'
     assert 'reaxff' not in captured.out
+
+
+def test_pip_install_cmd():
+    """Test pip install command to display."""
+    from aiida_registry.make_pages import get_pip_install_cmd
+
+    # aiida-crystal17 is beta version
+    assert '--pre' in get_pip_install_cmd(TEST_DATA['crystal17'])
+
+    # aiida-core is stable version
+    assert '--pre' not in get_pip_install_cmd(TEST_DATA['core'])
