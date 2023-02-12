204ca7c365ff08b7c7bf049498b2933b59f57ff2
Leopold Talirz <leopold.talirz@gmail.com>
2023-02-12 21:00:49 +0100

    apply new formatter/linter

--- aiida_registry/fetch_metadata.py
+++ aiida_registry/fetch_metadata.py
@@ -17,8 +17,13 @@ from typing import Optional
 import requests
 import yaml
 
-from . import (PLUGINS_FILE_ABS, PLUGINS_METADATA, REPORTER,
-               classifier_to_status, status_dict)
+from . import (
+    PLUGINS_FILE_ABS,
+    PLUGINS_METADATA,
+    REPORTER,
+    classifier_to_status,
+    status_dict,
+)
 from .parse_build_file import get_data_parser, identify_build_tool
 from .parse_pypi import PypiData, get_pypi_metadata
 from .utils import fetch_file
@@ -29,60 +34,61 @@ def get_hosted_on(url):
     try:
         requests.get(url, timeout=30)
     except Exception as exc:
-        raise ValueError(
-            "Unable to open 'code_home' url: '{}'".format(url)) from exc
+        raise ValueError("Unable to open 'code_home' url: '{}'".format(url)) from exc
 
     netloc = urllib.parse.urlparse(url).netloc
 
     # Remove port (if any)
-    netloc = netloc.partition(':')[0]
+    netloc = netloc.partition(":")[0]
 
     # Remove subdomains (this only works for domain suffixes of length 1!)
     # TODO: fix it for domains like yyy.co.uk  # pylint: disable=fixme
-    netloc = '.'.join(netloc.split('.')[-2:])
+    netloc = ".".join(netloc.split(".")[-2:])
 
     return netloc
 
 
-def complete_plugin_data(plugin_data: dict,
-                         fetch_pypi=True,
-                         fetch_pypi_wheel=True):  # pylint: disable=too-many-branches,too-many-statements
+def complete_plugin_data(
+    plugin_data: dict, fetch_pypi=True, fetch_pypi_wheel=True
+):  # pylint: disable=too-many-branches,too-many-statements
     """Update plugin data dictionary.
 
-      * add metadata, aiida_version and entrypoints from plugin_info
-      * add package_name if missing
-      * add hosted_on
-      & more
-     used for rendering.
+     * add metadata, aiida_version and entrypoints from plugin_info
+     * add package_name if missing
+     * add hosted_on
+     & more
+    used for rendering.
     """
-    if 'package_name' not in list(plugin_data.keys()):
-        plugin_data['package_name'] = plugin_data['name'].replace('-', '_')
+    if "package_name" not in list(plugin_data.keys()):
+        plugin_data["package_name"] = plugin_data["name"].replace("-", "_")
 
     REPORTER.info(f'{plugin_data["package_name"]}')
 
-    plugin_data['hosted_on'] = get_hosted_on(plugin_data['code_home'])
-    plugin_data.update({
-        'metadata': {},
-        'aiida_version': None,
-        'entry_points': None,
-    })
+    plugin_data["hosted_on"] = get_hosted_on(plugin_data["code_home"])
+    plugin_data.update(
+        {
+            "metadata": {},
+            "aiida_version": None,
+            "entry_points": None,
+        }
+    )
 
     # First try to get metadata from PyPI
     pypi_metadata: Optional[PypiData] = None
-    if fetch_pypi and is_pip_url_pypi(plugin_data.get('pip_url', '')):
-        pypi_metadata = get_pypi_metadata(plugin_data['pip_url'],
-                                          fetch_pypi_wheel)
+    if fetch_pypi and is_pip_url_pypi(plugin_data.get("pip_url", "")):
+        pypi_metadata = get_pypi_metadata(plugin_data["pip_url"], fetch_pypi_wheel)
     if pypi_metadata:
-        plugin_data['metadata'] = pypi_metadata.metadata
-        plugin_data['aiida_version'] = pypi_metadata.aiida_version
-        plugin_data['entry_points'] = pypi_metadata.entry_points
+        plugin_data["metadata"] = pypi_metadata.metadata
+        plugin_data["aiida_version"] = pypi_metadata.aiida_version
+        plugin_data["entry_points"] = pypi_metadata.entry_points
 
     # Now get missing data from the source repository
     if pypi_metadata is None or pypi_metadata.entry_points is None:
-        plugin_info_url = plugin_data.pop('plugin_info', None)
+        plugin_info_url = plugin_data.pop("plugin_info", None)
         if plugin_info_url is None:
             REPORTER.warn(
-                'Cannot fetch all data from PyPI and missing plugin_info key!')
+                "Cannot fetch all data from PyPI and missing plugin_info key!"
+            )
         else:
             # retrieve content of build file
             plugin_info_content = fetch_file(plugin_info_url)
@@ -96,32 +102,31 @@ def complete_plugin_data(plugin_data: dict,
                 if pypi_metadata is not None:
                     # we only need to get the entry points
                     data = get_data_parser(build_tool_name)(
-                        plugin_info_content,
-                        ep_only=True)  # , entry_points_only=True
-                    plugin_data['entry_points'] = data.entry_points
+                        plugin_info_content, ep_only=True
+                    )  # , entry_points_only=True
+                    plugin_data["entry_points"] = data.entry_points
                 else:
                     data = get_data_parser(build_tool_name)(
-                        plugin_info_content, ep_only=False)
-                    plugin_data['metadata'] = data.metadata
-                    plugin_data['aiida_version'] = data.aiida_version
-                    plugin_data['entry_points'] = data.entry_points
+                        plugin_info_content, ep_only=False
+                    )
+                    plugin_data["metadata"] = data.metadata
+                    plugin_data["aiida_version"] = data.aiida_version
+                    plugin_data["entry_points"] = data.entry_points
 
     # ensure entry points are not None
-    plugin_data['entry_points'] = plugin_data.get('entry_points') or {}
+    plugin_data["entry_points"] = plugin_data.get("entry_points") or {}
 
     # run validations
 
-    if plugin_data['name'] == 'aiida-core' and plugin_data['metadata'].get(
-            'version'):
-        plugin_data[
-            'aiida_version'] = f'=={plugin_data["metadata"]["version"]}'
-    if plugin_data.get('aiida_version') is None:
-        REPORTER.warn('AiiDA version not found')
+    if plugin_data["name"] == "aiida-core" and plugin_data["metadata"].get("version"):
+        plugin_data["aiida_version"] = f'=={plugin_data["metadata"]["version"]}'
+    if plugin_data.get("aiida_version") is None:
+        REPORTER.warn("AiiDA version not found")
 
     validate_dev_status(plugin_data)
 
-    if 'documentation_url' in plugin_data:
-        validate_doc_url(plugin_data['documentation_url'])
+    if "documentation_url" in plugin_data:
+        validate_doc_url(plugin_data["documentation_url"])
 
     validate_plugin_entry_points(plugin_data)
 
@@ -130,9 +135,12 @@ def complete_plugin_data(plugin_data: dict,
 
 def validate_dev_status(plugin_data: dict):
     """Validate the development_status key, potentially sourcing from classifiers."""
-    classifiers = plugin_data['metadata'].get(
-        'classifiers', []) if plugin_data.get('metadata', None) else []
-    if plugin_data['metadata'] and 'Framework :: AiiDA' not in classifiers:
+    classifiers = (
+        plugin_data["metadata"].get("classifiers", [])
+        if plugin_data.get("metadata", None)
+        else []
+    )
+    if plugin_data["metadata"] and "Framework :: AiiDA" not in classifiers:
         REPORTER.warn("Missing classifier 'Framework :: AiiDA'")
 
     # Read development status from plugin repo
@@ -140,66 +148,71 @@ def validate_dev_status(plugin_data: dict):
     for classifier in classifiers:
         if classifier in classifier_to_status:
             if development_status is not None:
-                REPORTER.warn(
-                    'Multiple development statuses found in classifiers')
+                REPORTER.warn("Multiple development statuses found in classifiers")
             development_status = classifier_to_status[classifier]
 
-    if development_status and 'development_status' in plugin_data and (
-            development_status != plugin_data['development_status']):
+    if (
+        development_status
+        and "development_status" in plugin_data
+        and (development_status != plugin_data["development_status"])
+    ):
         REPORTER.warn(
-            f'Development status in classifiers ({development_status}) '
+            f"Development status in classifiers ({development_status}) "
             f"does not match development_status in metadata ({plugin_data['development_status']})"
         )
 
     # prioritise development_status from plugins.yaml
-    if 'development_status' in plugin_data:
+    if "development_status" in plugin_data:
         REPORTER.warn(
-            '`development_status` key is deprecated. '
-            'Use PyPI Trove classifiers in the plugin repository instead.')
+            "`development_status` key is deprecated. "
+            "Use PyPI Trove classifiers in the plugin repository instead."
+        )
     else:
-        plugin_data['development_status'] = development_status or 'planning'
+        plugin_data["development_status"] = development_status or "planning"
 
     # note: for more validation, it might be sensible to switch to voluptuous
-    if plugin_data['development_status'] not in list(status_dict.keys()):
-        REPORTER.warn("Invalid development status '{}'".format(
-            plugin_data['development_status']))
+    if plugin_data["development_status"] not in status_dict:
+        REPORTER.warn(
+            "Invalid development status '{}'".format(plugin_data["development_status"])
+        )
 
 
 def validate_doc_url(url):
     """Validate that documentation URL provides valid HTTP response."""
     try:
-        response = requests.get(url)
-        response.raise_for_status(
-        )  # raise an exception for all 4xx/5xx errors
+        response = requests.get(url, timeout=60)
+        response.raise_for_status()  # raise an exception for all 4xx/5xx errors
     except Exception:  # pylint: disable=broad-except
-        REPORTER.warn('Unable to reach documentation URL: {}'.format(url))
+        REPORTER.warn("Unable to reach documentation URL: {}".format(url))
         REPORTER.debug(traceback.print_exc(file=sys.stdout))
 
 
 def validate_plugin_entry_points(plugin_data):
     """Validate that all entry points registered by the plugin start with the registered entry point root."""
-    if plugin_data['name'] == 'aiida-core':
+    if plugin_data["name"] == "aiida-core":
         return
 
-    if 'entry_point_prefix' in plugin_data:
-        entry_point_root = plugin_data['entry_point_prefix']
-        if not 'aiida_' + plugin_data['entry_point_prefix'].lower(
-        ) == plugin_data['package_name'].lower():
+    if "entry_point_prefix" in plugin_data:
+        entry_point_root = plugin_data["entry_point_prefix"]
+        if (
+            not "aiida_" + plugin_data["entry_point_prefix"].lower()
+            == plugin_data["package_name"].lower()
+        ):
             REPORTER.warn(
-                f"Prefix \'{plugin_data['entry_point_prefix']}\' does not follow naming convention."
+                f"Prefix '{plugin_data['entry_point_prefix']}' does not follow naming convention."
             )
     else:
         # plugin should not specify any entry points
-        entry_point_root = 'MISSING'
+        entry_point_root = "MISSING"
 
-    for ept_group, ept_list in (plugin_data['entry_points'] or {}).items():
+    for ept_group, ept_list in (plugin_data["entry_points"] or {}).items():
         # we only restrict aiida's entry point groups
-        if not ept_group.startswith('aiida.'):
+        if not ept_group.startswith("aiida."):
             continue
 
         for ept_string in ept_list:
             if not isinstance(ept_list, dict):
-                ept_string, _path = ept_string.split('=')
+                ept_string, _path = ept_string.split("=")
                 ept_string = ept_string.strip()
             if not ept_string.startswith(entry_point_root):
                 REPORTER.warn(
@@ -207,7 +220,7 @@ def validate_plugin_entry_points(plugin_data):
                 )
 
 
-PYPI_NAME_RE = re.compile(r'^[a-zA-Z0-9-_]+$')
+PYPI_NAME_RE = re.compile(r"^[a-zA-Z0-9-_]+$")
 
 
 def is_pip_url_pypi(string: str) -> bool:
@@ -218,7 +231,7 @@ def is_pip_url_pypi(string: str) -> bool:
 
 def fetch_metadata(filter_list=None, fetch_pypi=True, fetch_pypi_wheel=True):
     """Fetch metadata from PyPI and AiiDA-Plugins."""
-    with open(PLUGINS_FILE_ABS, encoding='utf8') as handle:
+    with open(PLUGINS_FILE_ABS, encoding="utf8") as handle:
         plugins_raw_data: dict = yaml.safe_load(handle)
 
     plugins_metadata = OrderedDict()
@@ -227,15 +240,14 @@ def fetch_metadata(filter_list=None, fetch_pypi=True, fetch_pypi_wheel=True):
         if filter_list and plugin_name not in filter_list:
             continue
         REPORTER.set_plugin_name(plugin_name)
-        plugin_data['name'] = plugin_name
+        plugin_data["name"] = plugin_name
         plugins_metadata[plugin_name] = complete_plugin_data(
-            plugin_data,
-            fetch_pypi=fetch_pypi,
-            fetch_pypi_wheel=fetch_pypi_wheel)
+            plugin_data, fetch_pypi=fetch_pypi, fetch_pypi_wheel=fetch_pypi_wheel
+        )
 
-    with open(PLUGINS_METADATA, 'w', encoding='utf8') as handle:
+    with open(PLUGINS_METADATA, "w", encoding="utf8") as handle:
         json.dump(plugins_metadata, handle, indent=2, sort_keys=True)
-    REPORTER.info(f'{PLUGINS_METADATA} dumped')
+    REPORTER.info(f"{PLUGINS_METADATA} dumped")
 
-    if os.environ.get('GITHUB_ACTIONS') == 'true':
-        print('::set-output name=error::' + '%0A'.join(REPORTER.warnings))
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        print("::set-output name=error::" + "%0A".join(REPORTER.warnings))
--- aiida_registry/utils.py
+++ aiida_registry/utils.py
@@ -8,25 +8,23 @@ import requests_cache
 
 from . import REPORTER
 
-if os.environ.get('CACHE_REQUESTS'):
+if os.environ.get("CACHE_REQUESTS"):
     # Set environment variable CACHE_REQUESTS to cache requests for 1 day for faster testing
     # e.g.: export CACHE_REQUESTS=1
-    requests_cache.install_cache('demo_cache', expire_after=60 * 60 * 24)
+    requests_cache.install_cache("demo_cache", expire_after=60 * 60 * 24)
 
 
-def fetch_file(file_url: str,
-               file_type: str = 'plugin info',
-               warn=True) -> str:
+def fetch_file(file_url: str, file_type: str = "plugin info", warn=True) -> str:
     """Fetch plugin info from a URL to a file."""
     try:
-        response = requests.get(file_url)
+        response = requests.get(file_url, timeout=60)
         # raise an exception for all 4xx/5xx errors
         response.raise_for_status()
     except Exception:  # pylint: disable=broad-except
         if warn:
             REPORTER.warn(
-                f'  > WARNING! Unable to retrieve {file_type} from: {file_url}'
+                f"  > WARNING! Unable to retrieve {file_type} from: {file_url}"
             )
             REPORTER.debug(traceback.format_exc())
         return None
-    return response.content.decode(response.encoding or 'utf8')
+    return response.content.decode(response.encoding or "utf8")
