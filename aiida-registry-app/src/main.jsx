bf99f67ac0317d3ea65957d603da742046e4ad7f
AhmedBasem <demhamesab@gmail.com>
2023-07-06 16:56:14 +0300

    update

--- aiida-registry-app/src/main.jsx
+++ aiida-registry-app/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
-import './index.css'
 const currentPath = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
